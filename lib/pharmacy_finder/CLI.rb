0a82ebfcadfc4c98d041793926a81076fd419aca
Donna <github email address>
2017-08-30 18:17:46 -0400

    created Store instances and call instance variables in the CLI

--- lib/pharmacy_finder/CLI.rb
+++ lib/pharmacy_finder/CLI.rb
@@ -8,13 +8,16 @@ class PharmacyFinder::CLI
 	Here are your local drugstores:
 	---------------------------
 		DOC
-		list_locations
+		list_stores
 		menu
 		goodbye
 	end
 
-	def list_locations
-		@locations = PharmacyFinder::Store.all
+	def list_stores
+		@stores = PharmacyFinder::Store.all
+		@stores.each.with_index(1) do |store, i|
+			puts "#{i}. #{store.name} - #{store.address} - #{store.distance}"
+		end
 	end
 
 	def menu
@@ -22,17 +25,13 @@ class PharmacyFinder::CLI
 		while choice != "exit"
 			puts "Select a location to get more info (or you can type list or exit):"
 			choice = gets.strip.downcase
-			case choice
-			when "1"
-				puts "More info about location 1"
-			when "2"
-				puts "More info about location 2"
-			when "3"
-				puts "More info about location 3"
-			when "list"
-				list_locations
+			
+			if choice.to_i > 0
+				puts @stores[choice.to_i-1]
+			elsif choice == "list"
+				list_stores
 			else
-				puts "Please try again, or type list or exit."
+				puts "Please try again, or type list or exit." unless choice == "exit"
 			end
 		end
 	end
--- lib/pharmacy_finder/store.rb
+++ lib/pharmacy_finder/store.rb
@@ -1,17 +1,16 @@
 class PharmacyFinder::Store
-	attr_accessor :name, :distance, :hours, :phone, :url
-	def initialize
-	end
+	attr_accessor :name, :address, :distance, :hours, :phone, :url
 
 	def self.all
 	#returns a bunch of instances of Store
-		puts <<-DOC
-	1. Rite Aid at 123 Exeter Rd.
-	2. CVS at 5 West Rd.
-	3. Target at 37 Portsmouth Dr.
-		DOC
+	# 	puts <<-DOC
+	# 1. Rite Aid at 123 Exeter Rd.
+	# 2. CVS at 5 West Rd.
+	# 3. Target at 37 Portsmouth Dr.
+	# 	DOC
 	store_1 = self.new
 	store_1.name = "Rite Aid"
+	store_1.address = "123 Exeter Rd."
 	store_1.distance = "2 mi."
 	store_1.hours = "8am - 9pm"
 	store_1.phone = "555-1234"
@@ -19,6 +18,7 @@ class PharmacyFinder::Store
 
 	store_2 = self.new
 	store_2.name = "CVS"
+	store_2.address = "5 West Rd."
 	store_2.distance = "5 mi."
 	store_2.hours = "8am - 9pm"
 	store_2.phone = "555-1236"
@@ -26,6 +26,7 @@ class PharmacyFinder::Store
 
 	store_3 = self.new
 	store_3.name = "Target"
+	store_3.address = "37 Portsmouth Dr."
 	store_3.distance = "7 mi."
 	store_3.hours = "7am - 11pm"
 	store_3.phone = "555-1238"
